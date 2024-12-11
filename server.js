480689dda98379ea8b99a77cff27c95bf90853e1
7amzawey <mohamedhamza123467890@gmail.com>
2024-12-11 15:33:55 +0200

    3

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(bodyParser.json());
 app.use('/', routes);
 
 app.listen(port, () => {
